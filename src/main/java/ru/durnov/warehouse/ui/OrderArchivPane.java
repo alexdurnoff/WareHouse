1492be10017184b72d055f88ca7328a9a99d179c
Alexej <door1975@yandex.ru>
2020-12-19 16:45:29 +0300

    Реализован поиск по дате и номеру в архиве накладных.

--- src/main/java/ru/durnov/warehouse/ui/OrderArchivPane.java
+++ src/main/java/ru/durnov/warehouse/ui/OrderArchivPane.java
@@ -5,15 +5,19 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import ru.durnov.warehouse.daoservice.EntityDaoService;
 import ru.durnov.warehouse.entity.DateStringComparator;
 import ru.durnov.warehouse.entity.Entity;
 import ru.durnov.warehouse.entity.Order;
+import ru.durnov.warehouse.entity.Product;
 import ru.durnov.warehouse.print.ViewForm;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class OrderArchivPane extends AbstractPane {
@@ -43,10 +47,50 @@ public class OrderArchivPane extends AbstractPane {
 
     @Override
     public void show() throws SQLException {
+        addSearchLineToPane();
         OrderComparator orderComparator = new OrderComparator();
         this.orderList = entityDaoService.getAllEntity();
         this.orderList.sort(orderComparator);
         if (this.orderList.size() > 20) trimOrderListLast20Orders();
+        addOrdersToPaneSortByDate();
+        setupPaneParameters();
+    }
+
+    private void addSearchLineToPane() {
+        HBox hBox = new HBox();
+        Button searchByDateButton = new Button("Поиск по дате");
+        TextField searchByDateTextField = new TextField();
+        Button searchByNumberButton = new Button("Поиск по номеру");
+        TextField searchByNumberTextField = new TextField();
+        searchByDateButton.setOnAction(ae -> {
+            try {
+                this.searchByDate(searchByDateTextField.getText());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+
+        searchByNumberButton.setOnAction(ae -> {
+            try {
+                this.searchByNumber(searchByNumberTextField.getText());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+        hBox.getChildren().add(searchByDateTextField);
+        hBox.getChildren().add(searchByDateButton);
+        hBox.getChildren().add(searchByNumberTextField);
+        hBox.getChildren().add(searchByNumberButton);
+        this.add(hBox, 0, rowCount, 4, 1);
+        rowCount++;
+    }
+
+    private void setupPaneParameters() {
+        this.setAlignment(Pos.CENTER);
+        this.setWidth(USE_PREF_SIZE);
+    }
+
+    private void addOrdersToPaneSortByDate() {
         List<String> dates = new ArrayList<>();
         makeDataList(dates);
         for (String date : dates) {
@@ -58,9 +102,6 @@ public class OrderArchivPane extends AbstractPane {
             addDateLineToPane(date);
             fillThisPaneOrders(dataOrderList);
         }
-        this.setGridLinesVisible(true);
-        this.setAlignment(Pos.CENTER);
-        this.setWidth(USE_PREF_SIZE);
     }
 
     private void addDateLineToPane(String date) {
@@ -133,5 +174,55 @@ public class OrderArchivPane extends AbstractPane {
             return null;
         }
     }
+
+    public void searchByDate(String date) throws SQLException {
+        this.getChildren().clear();
+        addReturnAllOrderListButton();
+        this.orderList = this.entityDaoService.getAllEntity();
+        List<Order> removedOrders = new ArrayList<>();
+        for (Entity entity : orderList) {
+            Order order = (Order) entity;
+            if(!(order.getDate().equals(date))) removedOrders.add(order);
+        }
+        this.orderList.removeAll(removedOrders);
+        OrderComparator orderComparator = new OrderComparator();
+        this.orderList.sort(orderComparator);
+        addOrdersToPaneSortByDate();
+        setupPaneParameters();
+    }
+
+    private void addReturnAllOrderListButton() {
+        HBox hBox = new HBox();
+        Button returnAllOrderListButton = new Button("Возврат к общему списку");
+        returnAllOrderListButton.setOnAction(ae -> {
+            try {
+                this.refresh();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+        hBox.getChildren().add(returnAllOrderListButton);
+        this.add(hBox, 0, 0, 4, 1);
+        rowCount++;
+    }
+
+    public void searchByNumber(String number) throws SQLException {
+        this.getChildren().clear();
+        addReturnAllOrderListButton();
+        this.orderList = this.entityDaoService.getAllEntity();
+        List<Order> removedOrders = new ArrayList<>();
+        for (Entity entity : orderList) {
+            Order order = (Order) entity;
+            String numberString = String.valueOf(order.getId());
+            if(!(numberString.equals(number)))removedOrders.add(order);
+        }
+        this.orderList.removeAll(removedOrders);
+        OrderComparator orderComparator = new OrderComparator();
+        this.orderList.sort(orderComparator);
+        addOrdersToPaneSortByDate();
+        setupPaneParameters();
+    }
+
+
 
 }
